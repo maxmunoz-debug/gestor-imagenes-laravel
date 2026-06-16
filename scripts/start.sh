2a029fbf9b081a2375d585762222a2f5ff56909d
maxmunoz-debug <max.munoz@tecsup.edu.pe>
2026-06-15 21:18:10 -0500

    Corregir instalación de composer en start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Instalando dependencias de Composer en producción..."
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --working-dir=/var/www/html
+
 echo "Optimizando la configuración de Laravel..."
 php artisan config:cache
 php artisan route:cache
